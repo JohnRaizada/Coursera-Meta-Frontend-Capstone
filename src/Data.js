8d7803b8053afa89a89200c5c1b27efb27359be4
John Raizada <indusinghkanchan@outlook.com>
2024-10-01 04:07:35 +0530

    redirected reservations to booking and updated favicon.ico to correct one

--- src/Data.js
+++ src/Data.js
@@ -33,7 +33,7 @@ export const navLinks = [
   { name: "Home", link: "/" },
   { name: "About", link: "/about" },
   { name: "Menu", link: "/menu" },
-  { name: "Reservations", link: "/reservations" },
+  { name: "Reservations", link: "/booking" },
   { name: "Order Online", link: "/order-online" },
   { name: "Login", link: "/login" },
 ];
